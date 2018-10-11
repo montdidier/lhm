a8b3dad4b7cd0b7ed7a20182011ddb047143c2be
Brian Buchalter <brian.buchalter@shopify.com>
2018-10-11 13:38:12 -0600

    Retry 20 times instead of 10

--- lib/lhm/sql_retry.rb
+++ lib/lhm/sql_retry.rb
@@ -52,7 +52,7 @@ module Lhm
         },
         multiplier: 1, # each successive interval grows by this factor
         base_interval: 1, # the initial interval in seconds between tries.
-        tries: 10, # Number of attempts to make at running your code block (includes initial attempt).
+        tries: 20, # Number of attempts to make at running your code block (includes initial attempt).
         rand_factor: 0, # percentage to randomize the next retry interval time
         max_elapsed_time: Float::INFINITY, # max total time in seconds that code is allowed to keep being retried
         on_retry: Proc.new do |exception, try_number, total_elapsed_time, next_interval|
